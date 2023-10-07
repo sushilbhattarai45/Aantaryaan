09bd20776e804ecc13869ea0e3016de98c2c85ac
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2023-10-07 21:57:55 +0545

    second page

--- aantaryaan/src/pages/img.js
+++ aantaryaan/src/pages/img.js
@@ -7,29 +7,83 @@ import "./PlanetSlider.css";
 const Planets = [
   {
     name: "Mercury",
-    img: "https://www.pngplay.com/wp-content/uploads/13/Mercury-PNG-Photos.png",
+    img: "../planets/Mercury.jpg",
   },
-
   {
     name: "Venus",
-    img: "https://p7.hiclipart.com/preview/682/838/406/earth-venus-planet-neptune-space-science-planet-mars.jpg",
+    img: "../planets/Venus.jpg",
   },
   {
     name: "Earth",
-    img: "https://clipart-library.com/image_gallery2/Earth-Free-Download-PNG.png",
+    img: "../planets/Earth.jpg",
   },
   {
     name: "Mars",
-    img: "https://img.freepik.com/premium-photo/mars-planet-isolated-white-background_374761-2815.jpg",
+    img: "../planets/Mars.jpg",
   },
   {
     name: "Jupiter",
-    img: "https://cdn.pixabay.com/photo/2015/09/16/11/15/jupiter-942558_960_720.jpg",
+    img: "../planets/Jupiter.webp",
+  },
+  {
+    name: "Saturn",
+    img: "../planets/Saturn.jpg",
+  },
+  {
+    name: "Uranus",
+    img: "../planets/Uranus.jpg",
+  },
+  {
+    name: "Neptune",
+    img: "../planets/Neptune.jpg",
+  },
+  {
+    name: "IO",
+    img: "../planets/Io.jpg",
+  },
+  {
+    name: "Europa",
+    img: "../planets/Europa.webp",
+  },
+  {
+    name: "Ganymede",
+    img: "../planets/Ganymede.jpg",
+  },
+  {
+    name: "Phobos",
+    img: "../planets/phobos.webp",
   },
 ];
 
+// const Planets = [
+//   {
+//     name: "Mercury",
+//     img: "https://www.pngplay.com/wp-content/uploads/13/Mercury-PNG-Photos.png",
+//   },
+
+//   {
+//     name: "Venus",
+//     img: "https://p7.hiclipart.com/preview/682/838/406/earth-venus-planet-neptune-space-science-planet-mars.jpg",
+//   },
+//   {
+//     name: "Earth",
+//     img: "https://clipart-library.com/image_gallery2/Earth-Free-Download-PNG.png",
+//   },
+//   {
+//     name: "Mars",
+//     img: "https://img.freepik.com/premium-photo/mars-planet-isolated-white-background_374761-2815.jpg",
+//   },
+//   {
+//     name: "Jupiter",
+//     img: "https://cdn.pixabay.com/photo/2015/09/16/11/15/jupiter-942558_960_720.jpg",
+//   },
+// ];
+
 const PlanetSlider = () => {
-  const [imgclick, setImgclick] = useState();
+  const [imgclick, setImgclick] = useState({
+    name: " Mercury",
+    img: "../planets/Mercury.jpg",
+  });
 
   const settings = {
     infinite: true,
@@ -54,7 +108,10 @@ const PlanetSlider = () => {
       <div>
         <Slider
           {...settings}
-          style={{ paddingTop: 50, scrollSnapType: "x mandatory" }}
+          style={{
+            paddingTop: 50,
+            scrollSnapType: "x mandatory",
+          }}
         >
           {Planets.map((planet, index) => (
             <div
@@ -64,6 +121,7 @@ const PlanetSlider = () => {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
+                outline: "none",
                 alignContent: "center",
                 alignSelf: "center",
               }}
@@ -72,11 +130,15 @@ const PlanetSlider = () => {
                 style={{
                   display: "flex",
                   alignSelf: "center",
+                  cursor: "pointer",
+
                   height: "200px",
                   width: "200px",
                   borderRadius: "50%",
                 }}
-                onClick={() => setImgclick(planet.name)}
+                onClick={() =>
+                  setImgclick({ name: planet.name, img: planet.img })
+                }
                 src={planet.img}
                 alt={`Planet ${index + 1}`}
               />
@@ -119,7 +181,7 @@ const PlanetSlider = () => {
               left: 10,
             }}
           >
-            {imgclick}
+            {imgclick.name}
           </span>
         </div>
         <div style={{ display: "flex", flex: 1 }}>
@@ -154,7 +216,82 @@ const PlanetSlider = () => {
           </span>
         </div>
       </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: 70,
+          right: 10,
+          zIndex: 1,
+          height: 100,
+          backdroundColor: "red",
+          width: 100,
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            bottom: 60,
+            width: 200,
+            height: 50,
+            borderRadius: 50,
+            right: 10,
+            zIndex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            alignContent: "center",
+            borderColor: "white",
+            borderWidth: 1,
+            border: "1px solid white",
+            backgroundColor: "black",
+          }}
+        >
+          <p
+            style={{
+              color: "white",
+              fontFamily: "Space Grotesk",
+              fontSize: "20px",
+              float: "left",
 
+              textAlign: "center",
+            }}
+          >
+            Activities
+          </p>
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            width: 200,
+            height: 50,
+            borderRadius: 50,
+            right: 10,
+            zIndex: 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            alignContent: "center",
+            borderColor: "white",
+            borderWidth: 1,
+            border: "1px solid white",
+            backgroundColor: "black",
+          }}
+        >
+          <p
+            style={{
+              color: "white",
+              fontFamily: "Space Grotesk",
+              fontSize: "20px",
+              float: "left",
+
+              textAlign: "center",
+            }}
+          >
+            Tour
+          </p>
+        </div>
+      </div>
       <div
         style={{
           position: "fixed",
@@ -164,11 +301,12 @@ const PlanetSlider = () => {
         }}
       >
         <img
-          src="https://clipart-library.com/image_gallery2/Earth-Free-Download-PNG.png"
+          src={imgclick.img}
           style={{
             overflow: "hidden",
-            height: "500px",
-            width: "500px",
+            height: "600px",
+            width: "600px",
+            borderRadius: "50%",
           }}
         />
       </div>
