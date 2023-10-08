86ef14f4c8f32de11c407e01291624029f93e27c
Sanskar <sskrlmsl@gmail.com>
2023-10-08 12:48:43 +0545

    Search Page

--- aantaryaan/src/pages/ActivitiesPage.js
+++ aantaryaan/src/pages/ActivitiesPage.js
@@ -40,15 +40,65 @@ const ActivitiesPage = () => {
         minHeight: "100vh",
         position: "relative",
         backgroundColor: "black",
-        padding: "48px",
+        padding: "32px",
         display: "block",
         //   overflowX: "hidden",
       }}
     >
-        <div>
+      <div
+        className="textContaainer"
+        style={{
+          marginTop:  '20px',
 
-        </div>
+          width: "100%",
+          position: "relative",
+          textAlign: "center",
+          flex: 1,
+          justifyContent: "center",
+          alignSelf: "center",
+          alignItems: "center",
+        }}
+      >
+        <span
+          style={{
+            textAlign: "center",
+            color: "white",
+            fontSize: "60px",
+            fontFamily: "Poppins",
+            fontWeight: "bold",
+          }}
+        >
+          What to do in Space?
+        </span>
+        <br />
+        <span
+          style={{
+            textAlign: "center",
+            color: "white",
+            fontSize: "32px",
+            fontWeight: 400,
+            fontFamily: "Space Grotesk",
+          }}
+        >
+          Don’t worry we have curated several activities
+        </span>
+        <br/>
+        <span
+          style={{
+            textAlign: "center",
+            color: "white",
+            fontSize: "32px",
+            fontWeight: 800,
+            fontFamily: "Space Grotesk",
+          }}
+        >
+          Just For You
+        </span>
+      </div>
+      <div style={{marginTop:'36px'}}>
+
       <SearchBar products={products} />
+      </div>
     </div>
   );
 };
